8ec58411d5d3228502c615b874aae4cdb9f01851
Chirag <54183439+10chirag10@users.noreply.github.com>
2023-04-19 21:30:20 +0530

    Delete faulty_finder.py

--- faulty_finder.py
+++
@@ -1,9 +0,0 @@
-import os, pydicom, tqdm
-root = './dataset_folder/train'
-for folders in tqdm.tqdm(os.listdir(root)):
-    images = os.path.join(root,folders)
-    for image in os.listdir(images):
-        try:
-                d = pydicom.dcmread(os.path.join(images,image)).pixel_array
-        except:
-                print(folders , " --------> " , image)
